45848c8d605783da0e7bde1e1ae6fc61982018c3
Ryan Gilbert <ryan.gilbert@formidable.com>
2020-01-09 22:18:31 -0700

    Convert components and pages to TypeScript

--- examples/1-with-urql-client/pages/index.tsx
+++ examples/1-with-urql-client/pages/index.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import Head from 'next/head';
+import { NextPageContext } from 'next';
 import { withUrqlClient } from 'next-urql';
 
-import PokemonList from '../components/pokemon_list';
+import PokémonList from '../components/pokemon_list';
 
-const Home = () => (
+const Home: React.FC = () => (
   <div>
     <Head>
       <title>Home</title>
       <link rel="icon" href="/static/favicon.ico" />
     </Head>
 
-    <PokemonList />
+    <PokémonList />
   </div>
 );
 
-export default withUrqlClient(ctx => {
+export default withUrqlClient((ctx: NextPageContext) => {
   return {
     url: 'https://graphql-pokemon.now.sh',
     fetchOptions: {
       headers: {
-        Authorization: `Bearer ${ctx.req.token}`,
+        Authorization: `Bearer ${ctx.req.headers.authorization}`,
       },
     },
   };
