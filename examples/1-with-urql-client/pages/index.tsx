ab305b84c8996388d9316bafff94db7f0e310423
Ryan Gilbert <ryan.gilbert@formidable.com>
2020-01-11 14:09:29 -0700

    Update example to use our custom NextContext

--- examples/1-with-urql-client/pages/index.tsx
+++ examples/1-with-urql-client/pages/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import { withUrqlClient } from 'next-urql';
-
-import { NextContext } from '../../../node_modules/@types/next/index';
+import { withUrqlClient, NextContextWithAppTree } from 'next-urql';
 import PokémonList from '../components/pokemon_list';
 
 const Home: React.FC = () => (
@@ -16,7 +14,7 @@ const Home: React.FC = () => (
   </div>
 );
 
-export default withUrqlClient((ctx: NextContext) => {
+export default withUrqlClient((ctx: NextContextWithAppTree) => {
   return {
     url: 'https://graphql-pokemon.now.sh',
     fetchOptions: {
