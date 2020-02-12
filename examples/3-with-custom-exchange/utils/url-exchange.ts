a736a28f3bae2964b8621737388f54d6003176cd
Ryan Gilbert <ryan.gilbert@formidable.com>
2020-02-12 14:21:04 -0500

    Add initial custom exchange example

---
+++ examples/3-with-custom-exchange/utils/url-exchange.ts
@@ -0,0 +1,18 @@
+import { ExchangeInput, Operation } from 'urql';
+import { map, pipe, Source } from 'wonka';
+
+export const urlExchange = ({ forward }: ExchangeInput) => {
+  return (operations$: Source<Operation>) => {
+    return pipe(
+      operations$,
+      map(op => {
+        console.log({ op });
+        if (process.browser) {
+          op.context.url = 'https://metaphysics-production.artsy.net/';
+        }
+        return op;
+      }),
+      forward,
+    );
+  };
+};
